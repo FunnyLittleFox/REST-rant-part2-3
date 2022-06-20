76f3ffeb79e45a95decd9fe8631476dd35d19434
Fox Flaherty <katyra14@gmail.com>
2022-06-20 14:05:34 -0700

    Adding 404 page view

--- index.js
+++ index.js
@@ -3,14 +3,22 @@ require('dotenv').config() //must always be at top
 const express = require('express')
 const app = express()
 
-app.get('/', function(req, res) {
-    res.send("Hello World!") // this must be start for homepage any others must come after
+app.set('view engine', 'jsx')
+
+//app.engine('jsx'. require('express-react-views').createEngine())
+//^ not working? Why?
+
+//app.use('/places', require('./controllers/places'))
+//^ not working? Why?
+
+app.get('/', (req, res) => {
+    res.render('home') // this must be start for homepage any others must come after
 })
 
-app.get('*',  function(req, res) {
-    res.status(404).send('<h1>404 Page</h1>')
+app.get('*',  (req, res) => {
+    res.render('error404')
 })
 
-app.listen(process.env.PORT, function(req, res) {
+app.listen(process.env.PORT, (req, res) => {
     console.log('Connected to Foxnet')
 }) // must always be at bottom
