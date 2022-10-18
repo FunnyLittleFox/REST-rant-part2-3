751bf42ee195becc211a8484328fea5e8128bf8d
Fox Flaherty <katyra14@gmail.com>
2022-10-18 11:57:12 -0700

    forgotten to push

--- controllers/places.js
+++ controllers/places.js
@@ -1,13 +1,15 @@
 const router = require('express').Router() //this should be at top
 
 router.get('/', (req, res) => {
-    let places = [{
+    let places = [
+        {
         name: 'H-Thai-ML',
         city: 'Seattle',
         state: 'WA',
         cuisines: 'Thai, Pan-Asian',
         pic: 'http://placekitten.com/250/250'
-      }, {
+      }, 
+      {
         name: 'Coding Cat Cafe',
         city: 'Phoenix',
         state: 'AZ',
