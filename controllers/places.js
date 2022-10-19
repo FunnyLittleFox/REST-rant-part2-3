01b97d7aa012cf3a9ffed04ebf7bd4d32f392f59
Fox Flaherty <katyra14@gmail.com>
2022-10-19 13:38:38 -0700

    show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -16,12 +16,24 @@ router.post('/', (req,res) => {
   res.resdirect('/places')
 })
 
-router.get('/', (req, res) => {
-    res.render('places/index', {places})
+router.get('/:id', (req, res) => {
+  res.render('places/show')
 })
 
 router.get('/new', (req, res) => {
   res.render('places/new')
 })
+
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  } else if (!places[id]) {
+      res.render('error404')
+  } else {
+      res.render('places/show', { place: places[id], id })
+  }
+})
+
 
 module.exports = router //this should be at bottom
